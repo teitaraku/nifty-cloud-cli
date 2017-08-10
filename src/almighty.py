96838caf2c86720a07855ef61ce4f8bbc0f93c8c
Kurata Yosuke <kurata.yosuke@nifty.co.jp>
2017-08-10 19:33:21 +0900

    :tada: cli can request anything

---
+++ src/almighty.py
@@ -0,0 +1,18 @@
+import click
+from niftycloud_request import NiftyCloudRequest
+from niftycloud_parser import NiftyCloudParser
+from itertools import zip_longest
+
+@click.command()
+@click.option('--action', help='[required] Action name')
+@click.option('--key', multiple=True)
+@click.option('--value', multiple=True)
+def almighty_request(action, key, value):
+
+    payload = {'Action': action}
+    for e in zip_longest(key, value):
+        key_, value_ = e
+        payload.update({key_: value_})
+
+    res = NiftyCloudRequest().request(payload)
+    NiftyCloudParser(res).simple()
